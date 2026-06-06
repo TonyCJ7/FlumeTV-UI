f881542d9998a0a129f01a0fc3036b0e2dc07edd
TonyCJ7 <tony.jacob@seera.sa>
2026-06-07 01:47:22 +0400

    test

--- components/core/ConfigEpgFieldset/ConfigEpgFieldset.tsx
+++ components/core/ConfigEpgFieldset/ConfigEpgFieldset.tsx
@@ -6,7 +6,7 @@ import { Styled } from "@/components/core/ConfigEpgFieldset/ConfigEpgFieldset.st
 
 type ConfigEpgFieldsetProps = Readonly<{
   legend: string;
-  epgEnabled: booleann;
+  epgEnabled: booleannn;
   children: ReactNode;
 }>;
 
